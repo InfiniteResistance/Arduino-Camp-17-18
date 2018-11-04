e6bcbecbaf05986d8f00e13c2acfec1cb3e297bb
Tassos Manganaris <tassos.manganaris@gmail.com>
2018-11-03 21:52:48 -0400

    Tracked several things.

---
+++ Testing/Test Arudino Programs/hallSensorTest-JM/hallSensorTest-JM.ino
@@ -0,0 +1,19 @@
+const int hallPin = 2;
+const int ledPin = 13;
+
+int hallSwitchState = 0;
+void setup() {
+  pinMode(hallPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  hallSwitchState = digitalRead(hallPin);
+  if(hallSwitchState == 0){
+    digitalWrite(ledPin, HIGH);  
+  }else{
+    digitalWrite(ledPin, LOW);  
+  }
+
+}
